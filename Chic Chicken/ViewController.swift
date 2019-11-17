6554590bff3fb7607c6fa30a7ae5d1a1977ea2fb
tariqhosny <tariqhosny@gmail.com>
2019-11-17 09:41:04 +0200

    Initial Commit

---
+++ Chic Chicken/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chic Chicken
+//
+//  Created by Tariq on 11/17/19.
+//  Copyright © 2019 Tariq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
